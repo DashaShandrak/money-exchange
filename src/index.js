2520c52477d2ff2545a9a91b05e5490c803412aa
DashaShandrak <d.shandrak@yandex.by>
2019-03-12 22:45:19 +0300

    add: add completed task money-exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency < 0) {
+        ERROR = {};
+        return(ERROR);
+    }
+    else if (currency >= 10000) {
+        ERROR = {error: "You are rich, my friend! We don't have so much coins for exchange"};
+        return (ERROR);
+    }
+    else {
+        money = {}; // create new object
+        H = currency / 50;
+        H = Math.floor(H);
+        if (H != 0) {
+            money.H = H;
+            currency = currency - 50 * H;
+        }
+        if (currency >= 25) {
+            money.Q = 1;
+            currency = currency - 25;
+        }
+        D = currency / 10;
+        D = Math.floor(D);
+        if (D !=0 ) {
+            money.D = D;
+            currency = currency - 10 * D;
+        }
+        if (currency >= 5) {
+            money.N = 1;
+            currency = currency - 5;
+        }
+        if (currency > 0) {
+            money.P = currency;
+        }
+        return(money);
+    }
 }
